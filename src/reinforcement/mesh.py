88bc1667be492ec77e29e9dc8e65dfea94ea9c2c
mcappone <mcappone@VDI0043.zit.bam.de>
2023-02-06 10:40:28 +0100

    moved all functions within create_concrete_slab out of the function and named them _function_name in order to discourage use of them

--- src/reinforcement/mesh.py
+++ src/reinforcement/mesh.py
@@ -8,6 +8,83 @@ from mpi4py import MPI
 from numpy.typing import ArrayLike
 import math
 
+#  correct number of concrete elements (defined by s) in order to fit the coice of n_x and n_y
+def _num_elems(min_amount_elems, reinf_elems):
+    reinf_elems -= 1
+    if min_amount_elems % reinf_elems == 0:
+        return min_amount_elems
+    else:
+        return int(np.ceil(min_amount_elems / reinf_elems)) * reinf_elems
+    
+def _reinforcement_points(Nx, x, y, addx, where, i, margin):
+    if where == "upper":
+        z = [1 - margin]
+    elif where == "lower":
+        z = [margin]
+    elif where == "both":
+        z = [margin, 1 - margin]
+
+    reinf_tags = []  # save tags to create lines later
+    ntp = gmsh.model.occ.getEntities(0)[-1][-1] + 1  # "next tag point"
+    x_cords = np.linspace(
+        x[0], x[-1], Nx
+    )  # contains all x (or y) coordinates where points have to be added
+    x_cords = np.repeat(
+        x_cords, 2
+    )  # doubles every element, because the same x value is added on two y values
+    y_cords = np.array(
+        y * Nx
+    )  # contains the two y - limits as often as necessary to add all points
+
+    for z_cord in z:
+        for (x_cord, y_cord) in zip(x_cords, y_cords):
+            i += 1
+            print("ntp+i", ntp + i)
+            # check if points for horizontal (x) or vertical (y) lines should be added
+            if addx == True:
+                gmsh.model.occ.addPoint(x_cord, y_cord, z_cord, tag=ntp + i)
+
+            else:
+                gmsh.model.occ.addPoint(y_cord, x_cord, z_cord, tag=ntp + i)
+            reinf_tags.append(ntp + i)
+    return reinf_tags, i
+
+# add lines which connect the reinforcement points
+def _reinforcement_lines(reinf_tags, ltp_Nx):
+    line_tags = []  # save line tags for physical group
+    ltl = gmsh.model.occ.getEntities(1)[-1][-1]  # "last tag line"
+    k = 0
+
+    for i, point_tag in enumerate(reinf_tags):
+        if i % 2 != 0:  # otherwise you will connect all points twice
+            k += 1  # to find the correct tags
+            gmsh.model.occ.addLine(reinf_tags[i - 1], point_tag, tag=ltl + k)
+            line_tags.append(ltl + k)
+    return line_tags
+
+# This function takes a msh file as input and separates the physical curve (reinforcement) from the physical volume (concrete) and saves each of them in a separated xdmf file
+def _create_xdmf(msh_file,xdmf_files):
+    def create_mesh(mesh, cell_type):
+        cells = mesh.get_cells_type(cell_type)
+        cell_data = mesh.get_cell_data("gmsh:physical", cell_type)
+        points = mesh.points
+        out_mesh = meshio.Mesh(
+            points=points,
+            cells={cell_type: cells},
+            cell_data={"name_to_read": [cell_data]},
+        )
+        return out_mesh
+
+    # Read mesh
+    msh = meshio.read(msh_file)
+
+    # Create and save one file containing the whole volume (hexahedra), and one file for the reinforcement lines
+    hexa_mesh = create_mesh(msh, "hexahedron")
+    line_mesh = create_mesh(msh, "line")
+
+    meshio.write(xdmf_files[0], hexa_mesh)
+    meshio.write(xdmf_files[1], line_mesh)
+
 def create_concrete_slab(
     point1: ArrayLike,
     point2: ArrayLike,
@@ -65,16 +142,8 @@ def create_concrete_slab(
     point1 = mymesh.occ.addPoint(x0, y0, z0)
     mymesh.occ.synchronize()
 
-    #  correct number of concrete elements (defined by s) in order to fit the coice of n_x and n_y
-    def num_elems(min_amount_elems, reinf_elems):
-        reinf_elems -= 1
-        if min_amount_elems % reinf_elems == 0:
-            return min_amount_elems
-        else:
-            return int(np.ceil(min_amount_elems / reinf_elems)) * reinf_elems
-
-    concrete_elems_x = num_elems(int((l - 2 * margin) / s), n_x + 1)
-    concrete_elems_y = num_elems(int((w - 2 * margin) / s), n_y + 1)
+    concrete_elems_x = _num_elems(int((l - 2 * margin) / s), n_x + 1)
+    concrete_elems_y = _num_elems(int((w - 2 * margin) / s), n_y + 1)
     n_elements_margin = math.ceil(margin/s)
     # extrude three times, point -> line (x-direction), line -> rectangle (y-direction), rectangle -> cuboid (z-direction)
     mymesh.occ.extrude(
@@ -117,66 +186,20 @@ def create_concrete_slab(
     # add points and lines where reinforcement is to be added (depending on n_x and n_y), function is called for x and y separately
     x = [x0 + margin, l - margin]
     y = [y0 + margin, w - margin]
-
-    def reinforcement_points(Nx, x, y, addx, where, i):
-        if where == "upper":
-            z = [1 - margin]
-        elif where == "lower":
-            z = [margin]
-        elif where == "both":
-            z = [margin, 1 - margin]
-
-        reinf_tags = []  # save tags to create lines later
-        ntp = mymesh.occ.getEntities(0)[-1][-1] + 1  # "next tag point"
-        x_cords = np.linspace(
-            x[0], x[-1], Nx
-        )  # contains all x (or y) coordinates where points have to be added
-        x_cords = np.repeat(
-            x_cords, 2
-        )  # doubles every element, because the same x value is added on two y values
-        y_cords = np.array(
-            y * Nx
-        )  # contains the two y - limits as often as necessary to add all points
-
-        for z_cord in z:
-            for (x_cord, y_cord) in zip(x_cords, y_cords):
-                i += 1
-                print("ntp+i", ntp + i)
-                # check if points for horizontal (x) or vertical (y) lines should be added
-                if addx == True:
-                    mymesh.occ.addPoint(x_cord, y_cord, z_cord, tag=ntp + i)
-
-                else:
-                    mymesh.occ.addPoint(y_cord, x_cord, z_cord, tag=ntp + i)
-                reinf_tags.append(ntp + i)
-        return reinf_tags, i
-
     i_points = 0
-    reinf_tags_x, i_points = reinforcement_points(
-        n_x + 1, x, y, True, where, i_points
+    
+    reinf_tags_x, i_points = _reinforcement_points(
+        n_x + 1, x, y, True, where, i_points, margin
     )  # for reinforcement in x-direction
 
     # before adding reinforcement in y-direction, it is necessary to know which was the last point that has been added before
     ltp_Nx = mymesh.occ.getEntities(0)[-1][-1]  # "last tag point"
-    reinf_tags_y, i_points = reinforcement_points(
-        n_y + 1, y, x, False, where, i_points
+    reinf_tags_y, i_points = _reinforcement_points(
+        n_y + 1, y, x, False, where, i_points, margin
     )  # for y-reinforcement
     reinf_tags = np.hstack((reinf_tags_x, reinf_tags_y))  # save all reinforcement tags
 
-    # add lines which connect the reinforcement points
-    def reinforcement_lines(reinf_tags, ltp_Nx):
-        line_tags = []  # save line tags for physical group
-        ltl = mymesh.occ.getEntities(1)[-1][-1]  # "last tag line"
-        k = 0
-
-        for i, point_tag in enumerate(reinf_tags):
-            if i % 2 != 0:  # otherwise you will connect all points twice
-                k += 1  # to find the correct tags
-                mymesh.occ.addLine(reinf_tags[i - 1], point_tag, tag=ltl + k)
-                line_tags.append(ltl + k)
-        return line_tags
-
-    line_tags = reinforcement_lines(reinf_tags, ltp_Nx)
+    line_tags = _reinforcement_lines(reinf_tags, ltp_Nx)
 
     # add new points to mesh by synchronizing
     mymesh.occ.synchronize()
@@ -193,30 +216,8 @@ def create_concrete_slab(
     gmsh.write(msh_filename)
     gmsh.finalize
     
-    # This function takes a msh file as input and separates the physical curve (reinforcement) from the physical volume (concrete) and saves each of them in a separated xdmf file
-    def create_xdmf(msh_file,xdmf_files):
-        def create_mesh(mesh, cell_type):
-            cells = mesh.get_cells_type(cell_type)
-            cell_data = mesh.get_cell_data("gmsh:physical", cell_type)
-            points = mesh.points
-            out_mesh = meshio.Mesh(
-                points=points,
-                cells={cell_type: cells},
-                cell_data={"name_to_read": [cell_data]},
-            )
-            return out_mesh
-
-        # Read mesh
-        msh = meshio.read(msh_file)
-
-        # Create and save one file containing the whole volume (hexahedra), and one file for the reinforcement lines
-        hexa_mesh = create_mesh(msh, "hexahedron")
-        line_mesh = create_mesh(msh, "line")
-
-        meshio.write(xdmf_files[0], hexa_mesh)
-        meshio.write(xdmf_files[1], line_mesh)
-
-    create_xdmf(msh_filename,xdmf_filenames)
+    # create two xdmf files (concrete & reinforcement)
+    _create_xdmf(msh_filename,xdmf_filenames)
 
 
 def read_xdmf(xdmf_files):
