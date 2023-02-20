ed4dc6d594dcb8c270d47e61a32f8e7c7a62dbef
srosenbu <sjard-mathis.rosenbusch@bam.de>
2023-02-20 16:51:58 +0100

    Rename file

--- test/analytical_solution.py
+++ test/analytical_solution.py
@@ -1,22 +1,70 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Feb 20 14:10:15 2023
+Created on Mon Feb 20 13:32:23 2023
 
 @author: mcappone
 """
+import numpy as np
 
-from test_analytical_solution import analytical_solution
+class analytical_solution:
+    def __init__(self,q0,B,H,L,zr,params_rebar,params_concrete):
+        '''
+        Parameters
+        ----------
+        q0: float  
+            Distributed force (applied to the top of the slab over the whole width).
+        B: float
+           Width of the cuboid.
+        H: float
+           Height of the cuboid.
+        L: float
+           Length of the cuboid.
+        zr: float 
+            Distance from z=0 to the rebars.
+        params_rebar: Dict
+            Contains the following information on the reinforcement bars:
+            "Youngs_Modulus_steel": float, "density_steel": float, "amount": int - amount of rebars, "area":float - area of one rebar.
+        params_concrete: Dict
+            Contains the following information of the concrete:
+            "Youngs_Modulus_concrete": float, "density_concrete": float
 
-#example on how to derive an analytical solution for the three-point bending test
-q0 = 10
-B = 1
-H = 2
-L = 1.5
-zr = 0.1
-params_rebar = {"Youngs_Modulus_steel":10, "density_steel":5, "amount": 2, "area":3}
-params_concrete = {"Youngs_Modulus_concrete":5,"density_concrete":3}
-y0 = 0.5
-example = analytical_solution(q0,B,H,L,zr,params_rebar,params_concrete)
-w = example.evaluate(y0)
-print(w)
+        Returns
+        -------
+
+        '''
+        E_s = params_rebar["Youngs_Modulus_steel"]
+        rho_s = params_rebar["density_steel"]
+        i = params_rebar["amount"]
+        A_r = params_rebar["area"]
+        E_c = params_concrete["Youngs_Modulus_concrete"]
+        rho_c = params_concrete["density_concrete"]
+        V_c = B*H*L
+        V_s = i*A_r*L
+        zg = (rho_c*V_c*H/2 + rho_s*V_s*zr) / (rho_c*V_c + rho_s*V_s)
+        I_c = B*H**3/12 + (H/2 - zg)**2*B*H
+        I_s = i*A_r*(A_r/(4*np.pi) + (zr-zg)**2)
+        self.EI = E_c*I_c + E_s*I_s
+        self.q0 = q0
+        self.B = B
+        
+        
+    
+    def evaluate(self,y0):
+        '''
+        Parameters
+        ----------
+        y0 : float
+            position at which displacement is to be evaluated.
+
+        Returns
+        -------
+        w_y : float
+            displacement in vertical direction at horizontal position y0.
+
+        '''
+        w_y = 1/(24*self.EI)*self.q0*(y0**4 -2*self.B*y0**3+self.B**3*y0)
+        return w_y
+
+
+
